22645acc32f343b39daa6775b018b7c0c75b0e90
scott-m-king <scott-m-king@outlook.com>
2020-08-13 23:53:13 -0700

    Added cards and title to home screen

--- web/src/Home.tsx
+++ web/src/Home.tsx
@@ -4,15 +4,35 @@ import {
   makeStyles,
   Theme,
   createStyles,
-  Toolbar,
   Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  Grid,
+  Paper,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    content: {
-      flexGrow: 1,
+    root: {
+      minWidth: 540,
+    },
+    bullet: {
+      display: "inline-block",
+      margin: "0 2px",
+      transform: "scale(0.8)",
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    paper: {
       padding: theme.spacing(3),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
     },
   })
 );
@@ -21,6 +41,7 @@ export const Home = () => {
   const { data, loading } = useMeQuery();
   let body: any = null;
   const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
 
   if (loading) {
     body = "loading...";
@@ -30,8 +51,99 @@ export const Home = () => {
     body = "undefined";
   }
   return (
-    <main className={classes.content}>
-      <Typography variant="h4">Welcome back, {body}</Typography>
-    </main>
+    <>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs>
+            <Paper className={classes.paper}>
+              <Typography variant="h4">Welcome back, {body}</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs>
+            <Card className={classes.root}>
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  Word of the Day
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                  adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card className={classes.root}>
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  Word of the Day
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                  adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs>
+            <Card className={classes.root}>
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="textSecondary"
+                  gutterBottom
+                >
+                  Word of the Day
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                  adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+    </>
   );
 };
