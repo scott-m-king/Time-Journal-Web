e6503d0fa51f478a54a5bf148f9e7bd56a662af8
scott-m-king <scott-m-king@outlook.com>
2020-08-26 15:24:13 -0700

    Added responsive grid to category page
    Co-Authored-By: Scott <46171819+scott-m-king@users.noreply.github.com>

--- web/src/App/CategoryList.tsx
+++ web/src/App/CategoryList.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import { Grid, makeStyles } from "@material-ui/core";
+import { CategoryCard } from "../components/CategoryCard";
 
-interface CategoryListProps {}
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  root: {
+    flexGrow: 1,
+  },
+});
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const CategoryList = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Typography variant="h4">Categories</Typography>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h4">Categories</Typography>
+        </Grid>
+        {array.map((num, index) => {
+          return (
+            <Grid item key={index} xs={12} sm={12} md={6} lg={4} xl={4}>
+              <CategoryCard />
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 };
---
+++ web/src/components/CategoryCard.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    // maxWidth: 345,
+  },
+});
+
+export const CategoryCard = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://s18671.pcdn.co/wp-content/uploads/2019/06/20181114-LIZARD-FENSTON-08.jpg"
+          title={`Contemplative Reptile number`}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
