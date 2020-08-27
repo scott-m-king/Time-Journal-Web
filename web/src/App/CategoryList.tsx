5ad2f73bb4c96b0541058c2e07be05a8aaf34227
scott-m-king <scott-m-king@outlook.com>
2020-08-26 20:24:49 -0700

    Layout for category page
    Co-Authored-By: Scott <46171819+scott-m-king@users.noreply.github.com>

--- web/src/App/CategoryList.tsx
+++ web/src/App/CategoryList.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, makeStyles, FormControlLabel, Switch } from "@material-ui/core";
 import AddCircleRoundedIcon from "@material-ui/icons/AddCircleRounded";
 import { CategoryLane } from "../components/Categories/CategoryLane";
 import { CategoryData } from "../components/Categories/CategoryData";
+import { CategoryTable } from "../components/Categories/CategoryTable";
+import { CategoryCalendar } from "../components/Categories/CategoryCalendar";
 
 const useStyles = makeStyles({
   root: {
@@ -13,6 +15,11 @@ const useStyles = makeStyles({
 
 export const CategoryList = () => {
   const classes = useStyles();
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
 
   return (
     <div className={classes.root}>
@@ -29,7 +36,13 @@ export const CategoryList = () => {
           <CategoryLane />
         </Grid>
         <Grid item xs={8} sm={8} md={8} lg={8} xl={8}>
-          <CategoryData />
+          <FormControlLabel
+            control={<Switch checked={checked} onChange={handleChange} />}
+            label={checked ? "Switch to Pie Chart" : "Switch to Calendar"}
+          />
+          {checked ? <CategoryCalendar /> : <CategoryData />}
+          <br />
+          <CategoryTable />
         </Grid>
       </Grid>
     </div>
--- web/src/components/Categories/CategoryLane.tsx
+++ web/src/components/Categories/CategoryLane.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Paper, Grid } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
 import { CategoryCard } from "./CategoryCard";
 
 interface LaneProps {}
@@ -7,8 +7,24 @@ interface LaneProps {}
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export const CategoryLane: React.FC<LaneProps> = ({}) => {
+  const [windowHeight, setWindowHeight] = useState(0);
+  const MAX_HEIGHT = windowHeight - 165;
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowDimensions);
+    updateWindowDimensions();
+  }, []);
+
+  const updateWindowDimensions = () => {
+    setWindowHeight(window.innerHeight);
+  };
+
   return (
-    <Grid container spacing={1}>
+    <Grid
+      container
+      spacing={1}
+      style={{ maxHeight: MAX_HEIGHT, overflow: "auto" }}
+    >
       {array.map((num, index) => {
         return (
           <Grid item xs={12}>
