cdc35bbb4d242f63133b6488c434bc7efbacb71b
scott-m-king <scott-m-king@outlook.com>
2020-09-02 00:42:28 -0700

    Tentative layout for dashboard complete
    Co-Authored-By: Scott <46171819+scott-m-king@users.noreply.github.com>

--- web/src/App/Dashboard.tsx
+++ web/src/App/Dashboard.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { useMeQuery } from "../generated/graphql";
+import {
+  useMeQuery,
+  GetUserCategoriesQuery,
+  GetUserCategoriesDocument,
+  GetAllUserEntriesQuery,
+  GetAllUserEntriesDocument,
+  useCreateEntryMutation,
+  useEditEntryMutation,
+} from "../generated/graphql";
 import {
   makeStyles,
   Theme,
@@ -7,11 +15,15 @@ import {
   Typography,
   Card,
   CardContent,
-  CardActions,
-  Button,
   Grid,
+  Paper,
 } from "@material-ui/core";
 import styled from "styled-components";
+import { CreateEntryForm } from "../Forms/CreateEntryForm";
+import { JournalEntry } from "../redux/types";
+import { ResponsiveCalendar } from "@nivo/calendar";
+import { ResponsiveLine } from "@nivo/line";
+import { data2, data3 } from "../components/Categories/calendarData2";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,7 +31,7 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
     cards: {
-      minWidth: 275,
+      textAlign: "center",
     },
     bullet: {
       display: "inline-block",
@@ -29,13 +41,14 @@ const useStyles = makeStyles((theme: Theme) =>
     title: {
       fontSize: 14,
     },
-    pos: {
+    top: {
+      marginTop: 12,
+    },
+    bot: {
       marginBottom: 12,
     },
     paper: {
-      padding: theme.spacing(3),
-      textAlign: "center",
-      color: theme.palette.text.secondary,
+      padding: theme.spacing(2),
     },
   })
 );
@@ -50,6 +63,8 @@ export const Dashboard = () => {
   const { data, loading } = useMeQuery();
   let body: any = null;
   const classes = useStyles();
+  const [createEntry] = useCreateEntryMutation();
+  const [editEntry] = useEditEntryMutation();
 
   if (loading) {
     body = "loading...";
@@ -58,6 +73,98 @@ export const Dashboard = () => {
   } else {
     body = "";
   }
+
+  const handleSubmit = async (data: JournalEntry) => {
+    try {
+      const response = await createEntry({
+        variables: {
+          categoryId: data.categoryId,
+          title: data.title,
+          notes: data.notes,
+          duration: data.duration,
+          date: getCurrentDayTimestamp(new Date(data.date)),
+        },
+        update: (store, { data }) => {
+          if (!data) {
+            return null;
+          }
+          store.writeQuery<GetUserCategoriesQuery>({
+            query: GetUserCategoriesDocument,
+            data: {
+              getUserCategories: data.createEntry.categories,
+            },
+          });
+          store.writeQuery<GetAllUserEntriesQuery>({
+            query: GetAllUserEntriesDocument,
+            data: {
+              getAllUserEntries: data.createEntry.entries,
+            },
+          });
+        },
+      });
+      if (!response) {
+        alert("Failed to add.");
+        return;
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const handleEdit = async (id: number, data: JournalEntry) => {
+    try {
+      const response = await editEntry({
+        variables: {
+          id: id,
+          categoryId: data.categoryId,
+          title: data.title,
+          notes: data.notes,
+          duration: data.duration,
+          date: getCurrentDayTimestamp(new Date(data.date)),
+        },
+        update: (store, { data }) => {
+          if (!data) {
+            return null;
+          }
+          store.writeQuery<GetUserCategoriesQuery>({
+            query: GetUserCategoriesDocument,
+            data: {
+              getUserCategories: data.editEntry.categories,
+            },
+          });
+          store.writeQuery<GetAllUserEntriesQuery>({
+            query: GetAllUserEntriesDocument,
+            data: {
+              getAllUserEntries: data.editEntry.entries,
+            },
+          });
+        },
+      });
+      if (!response) {
+        alert("Failed to edit.");
+        return;
+      }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const getCurrentDayTimestamp = (d: Date) => {
+    console.log(d);
+    return new Date(
+      Date.UTC(
+        d.getFullYear(),
+        d.getMonth(),
+        d.getDate(),
+        d.getHours(),
+        d.getMinutes(),
+        d.getSeconds()
+      )
+    )
+      .toUTCString()
+      .slice(0, 16);
+  };
+
   return (
     <Layout>
       <div className={classes.root}>
@@ -65,85 +172,194 @@ export const Dashboard = () => {
           <Grid item xs={12}>
             <Typography variant="h4">Welcome back, {body}</Typography>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Card className={classes.cards}>
-              <CardContent>
-                <Typography
-                  className={classes.title}
-                  color="textSecondary"
-                  gutterBottom
-                >
-                  Word of the Day
-                </Typography>
-                <Typography variant="h5" component="h2">
-                  Categories
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                  adjective
-                </Typography>
-                <Typography variant="body2" component="p">
-                  well meaning and kindly.
-                  <br />
-                  {'"a benevolent smile"'}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
+          <Grid item xs={3} sm={3} md={3} lg={3} xl={3}>
+            <Paper className={classes.paper}>
+              <Typography variant="h5" component="h2">
+                Quick-add Journal Entry
+              </Typography>
+              <CreateEntryForm onEdit={handleEdit} onSubmit={handleSubmit} />
+            </Paper>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Card className={classes.cards}>
-              <CardContent>
-                <Typography
-                  className={classes.title}
-                  color="textSecondary"
-                  gutterBottom
-                >
-                  Word of the Day
-                </Typography>
-                <Typography variant="h5" component="h2">
-                  Entries
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                  adjective
-                </Typography>
-                <Typography variant="body2" component="p">
-                  well meaning and kindly.
-                  <br />
-                  {'"a benevolent smile"'}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
+          <Grid item xs={9}>
+            <Grid container spacing={3}>
+              <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                <Card className={classes.cards}>
+                  <CardContent>
+                    <Typography
+                      variant="h5"
+                      component="h2"
+                      className={classes.top}
+                    >
+                      <b>16</b> Entries
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      component="p"
+                      className={classes.bot}
+                    >
+                      Entered in total
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                <Card className={classes.cards}>
+                  <CardContent>
+                    <Typography
+                      variant="h5"
+                      component="h2"
+                      className={classes.top}
+                    >
+                      <b>5</b> Entries
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      component="p"
+                      className={classes.bot}
+                    >
+                      Entered in the last week
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                <Card className={classes.cards}>
+                  <CardContent>
+                    <Typography
+                      variant="h5"
+                      component="h2"
+                      className={classes.top}
+                    >
+                      <b>School</b>
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      component="p"
+                      className={classes.bot}
+                    >
+                      Most time spent in this category
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <Card className={classes.cards}>
+                  <div style={{ position: "relative", height: 438 }}>
+                    <div
+                      style={{
+                        position: "absolute",
+                        width: "100%",
+                        height: "100%",
+                      }}
+                    >
+                      <ResponsiveLine
+                        data={data3}
+                        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                        xScale={{ type: "point" }}
+                        yScale={{
+                          type: "linear",
+                          min: "auto",
+                          max: "auto",
+                          stacked: true,
+                          reverse: false,
+                        }}
+                        axisTop={null}
+                        axisRight={null}
+                        axisBottom={{
+                          orient: "bottom",
+                          tickSize: 5,
+                          tickPadding: 5,
+                          tickRotation: 0,
+                          legend: "transportation",
+                          legendOffset: 36,
+                          legendPosition: "middle",
+                        }}
+                        axisLeft={{
+                          orient: "left",
+                          tickSize: 5,
+                          tickPadding: 5,
+                          tickRotation: 0,
+                          legend: "count",
+                          legendOffset: -40,
+                          legendPosition: "middle",
+                        }}
+                        colors={{ scheme: "nivo" }}
+                        pointSize={10}
+                        pointColor={{ theme: "background" }}
+                        pointBorderWidth={2}
+                        pointBorderColor={{ from: "serieColor" }}
+                        pointLabel="y"
+                        pointLabelYOffset={-12}
+                        useMesh={true}
+                        legends={[
+                          {
+                            anchor: "bottom-right",
+                            direction: "column",
+                            justify: false,
+                            translateX: 100,
+                            translateY: 0,
+                            itemsSpacing: 0,
+                            itemDirection: "left-to-right",
+                            itemWidth: 80,
+                            itemHeight: 20,
+                            itemOpacity: 0.75,
+                            symbolSize: 12,
+                            symbolShape: "circle",
+                            symbolBorderColor: "rgba(0, 0, 0, .5)",
+                            effects: [
+                              {
+                                on: "hover",
+                                style: {
+                                  itemBackground: "rgba(0, 0, 0, .03)",
+                                  itemOpacity: 1,
+                                },
+                              },
+                            ],
+                          },
+                        ]}
+                      />
+                    </div>
+                  </div>
+                </Card>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
             <Card className={classes.cards}>
-              <CardContent>
-                <Typography
-                  className={classes.title}
-                  color="textSecondary"
-                  gutterBottom
+              <div style={{ position: "relative", height: 275 }}>
+                <div
+                  style={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                  }}
                 >
-                  Word of the Day
-                </Typography>
-                <Typography variant="h5" component="h2">
-                  Overview
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                  adjective
-                </Typography>
-                <Typography variant="body2" component="p">
-                  well meaning and kindly.
-                  <br />
-                  {'"a benevolent smile"'}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
+                  <ResponsiveCalendar
+                    data={data2()}
+                    from="2020-02-01"
+                    to="2020-12-01"
+                    emptyColor="#eeeeee"
+                    colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+                    margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+                    yearSpacing={40}
+                    monthBorderColor="#ffffff"
+                    dayBorderWidth={2}
+                    dayBorderColor="#ffffff"
+                    legends={[
+                      {
+                        anchor: "bottom-right",
+                        direction: "row",
+                        translateY: 36,
+                        itemCount: 4,
+                        itemWidth: 42,
+                        itemHeight: 36,
+                        itemsSpacing: 14,
+                        itemDirection: "right-to-left",
+                      },
+                    ]}
+                  />
+                </div>
+              </div>
             </Card>
           </Grid>
         </Grid>
--- web/src/components/Categories/calendarData2.ts
+++ web/src/components/Categories/calendarData2.ts
@@ -2,7 +2,7 @@ export const data2 = () => {
   let arr = [];
   for (let i = 0; i < 200; i++) {
     arr.push({
-      day: randomDate(new Date(2014, 12, 1), new Date(2016, 12, 31)),
+      day: randomDate(new Date(2020, 1, 1), new Date(2021, 1, 1)),
       value: i,
     });
   }
@@ -34,3 +34,276 @@ function getCurrentDayTimestamp(d: Date) {
     .toISOString()
     .slice(0, 10);
 }
+
+export const data3 = [
+  {
+    id: "japan",
+    color: "",
+    data: [
+      {
+        x: "plane",
+        y: 149,
+      },
+      {
+        x: "helicopter",
+        y: 251,
+      },
+      {
+        x: "boat",
+        y: 133,
+      },
+      {
+        x: "train",
+        y: 202,
+      },
+      {
+        x: "subway",
+        y: 274,
+      },
+      {
+        x: "bus",
+        y: 264,
+      },
+      {
+        x: "car",
+        y: 119,
+      },
+      {
+        x: "moto",
+        y: 169,
+      },
+      {
+        x: "bicycle",
+        y: 2,
+      },
+      {
+        x: "horse",
+        y: 150,
+      },
+      {
+        x: "skateboard",
+        y: 197,
+      },
+      {
+        x: "others",
+        y: 155,
+      },
+    ],
+  },
+  {
+    id: "france",
+    color: "",
+    data: [
+      {
+        x: "plane",
+        y: 181,
+      },
+      {
+        x: "helicopter",
+        y: 280,
+      },
+      {
+        x: "boat",
+        y: 34,
+      },
+      {
+        x: "train",
+        y: 126,
+      },
+      {
+        x: "subway",
+        y: 9,
+      },
+      {
+        x: "bus",
+        y: 198,
+      },
+      {
+        x: "car",
+        y: 76,
+      },
+      {
+        x: "moto",
+        y: 138,
+      },
+      {
+        x: "bicycle",
+        y: 50,
+      },
+      {
+        x: "horse",
+        y: 20,
+      },
+      {
+        x: "skateboard",
+        y: 254,
+      },
+      {
+        x: "others",
+        y: 207,
+      },
+    ],
+  },
+  {
+    id: "us",
+    color: "",
+    data: [
+      {
+        x: "plane",
+        y: 14,
+      },
+      {
+        x: "helicopter",
+        y: 200,
+      },
+      {
+        x: "boat",
+        y: 65,
+      },
+      {
+        x: "train",
+        y: 234,
+      },
+      {
+        x: "subway",
+        y: 235,
+      },
+      {
+        x: "bus",
+        y: 112,
+      },
+      {
+        x: "car",
+        y: 125,
+      },
+      {
+        x: "moto",
+        y: 239,
+      },
+      {
+        x: "bicycle",
+        y: 54,
+      },
+      {
+        x: "horse",
+        y: 165,
+      },
+      {
+        x: "skateboard",
+        y: 174,
+      },
+      {
+        x: "others",
+        y: 54,
+      },
+    ],
+  },
+  {
+    id: "germany",
+    color: "",
+    data: [
+      {
+        x: "plane",
+        y: 228,
+      },
+      {
+        x: "helicopter",
+        y: 3,
+      },
+      {
+        x: "boat",
+        y: 38,
+      },
+      {
+        x: "train",
+        y: 176,
+      },
+      {
+        x: "subway",
+        y: 207,
+      },
+      {
+        x: "bus",
+        y: 216,
+      },
+      {
+        x: "car",
+        y: 38,
+      },
+      {
+        x: "moto",
+        y: 73,
+      },
+      {
+        x: "bicycle",
+        y: 244,
+      },
+      {
+        x: "horse",
+        y: 228,
+      },
+      {
+        x: "skateboard",
+        y: 67,
+      },
+      {
+        x: "others",
+        y: 126,
+      },
+    ],
+  },
+  {
+    id: "norway",
+    color: "",
+    data: [
+      {
+        x: "plane",
+        y: 224,
+      },
+      {
+        x: "helicopter",
+        y: 262,
+      },
+      {
+        x: "boat",
+        y: 200,
+      },
+      {
+        x: "train",
+        y: 182,
+      },
+      {
+        x: "subway",
+        y: 184,
+      },
+      {
+        x: "bus",
+        y: 110,
+      },
+      {
+        x: "car",
+        y: 157,
+      },
+      {
+        x: "moto",
+        y: 156,
+      },
+      {
+        x: "bicycle",
+        y: 261,
+      },
+      {
+        x: "horse",
+        y: 230,
+      },
+      {
+        x: "skateboard",
+        y: 95,
+      },
+      {
+        x: "others",
+        y: 257,
+      },
+    ],
+  },
+];
