e11d216e4777de8b79d0e4b9df1ebeb2352c9757
scott-m-king <scott-m-king@outlook.com>
2020-09-04 21:58:02 -0700

    Finished styling on LineGraph
    Co-Authored-By: Scott <46171819+scott-m-king@users.noreply.github.com>

--- web/src/components/Dashboard/LineGraphWidget.tsx
+++ web/src/components/Dashboard/LineGraphWidget.tsx
@@ -1,23 +1,32 @@
-import React, { useEffect } from "react";
-import {
-  DataObject,
-  data2,
-  generateData,
-} from "../../Functions/dataProcessing";
+import React, { useState } from "react";
+import { DataObject } from "../../Functions/dataProcessing";
 import { ResponsiveLine } from "@nivo/line";
-import { Typography } from "@material-ui/core";
+import { Typography, useTheme, Card } from "@material-ui/core";
+import { useMeQuery } from "../../generated/graphql";
+import { blueGrey } from "@material-ui/core/colors";
+import Brightness1Icon from "@material-ui/icons/Brightness1";
 
 interface LineGraphProps {
   data: DataObject[] | undefined;
 }
 
 export const LineGraphWidget: React.FC<LineGraphProps> = ({ data }) => {
+  const muiTheme = useTheme();
+  const { loading, data: meData } = useMeQuery();
+  const [colors, setColors] = useState<Array<any>>([]);
+  const [active, setActive] = useState<boolean>(false);
+
+  const theme = {
+    textColor: muiTheme.palette.text.primary,
+  };
+
   return (
     <>
       {data ? (
         <ResponsiveLine
           // data={generateData()}
           data={data!}
+          theme={theme as any}
           margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
           xScale={{ type: "point" }}
           yScale={{
@@ -48,15 +57,56 @@ export const LineGraphWidget: React.FC<LineGraphProps> = ({ data }) => {
             legendOffset: -40,
             legendPosition: "middle",
           }}
-          colors={{ scheme: "paired" }}
+          colors={
+            active
+              ? colors
+              : {
+                  scheme:
+                    meData && meData.me && meData?.me.theme === "light"
+                      ? "category10"
+                      : "nivo",
+                }
+          }
           pointSize={10}
           pointColor={{ theme: "background" }}
           pointBorderWidth={2}
           enableSlices="x"
+          sliceTooltip={({ slice }) => (
+            <Card
+              style={{
+                padding: 6,
+                backgroundColor:
+                  meData && meData.me && meData?.me.theme === "light"
+                    ? "white"
+                    : blueGrey[800],
+              }}
+            >
+              {slice.points.map((point, index) => (
+                <div
+                  key={index}
+                  style={{
+                    padding: 2,
+                    backgroundColor:
+                      meData && meData.me && meData?.me.theme === "light"
+                        ? "white"
+                        : blueGrey[800],
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <Brightness1Icon
+                    style={{ fontSize: 12, color: point.serieColor }}
+                  />
+                  {"\u00A0\u00A0"}
+                  <b>{point.serieId}</b>: {point.data.yFormatted} mins
+                </div>
+              ))}
+            </Card>
+          )}
           pointBorderColor={{ from: "serieColor" }}
           pointLabel="y"
           pointLabelYOffset={-12}
-          useMesh={true}
+          useMesh={false}
           legends={[
             {
               anchor: "right",
@@ -69,9 +119,25 @@ export const LineGraphWidget: React.FC<LineGraphProps> = ({ data }) => {
               itemWidth: 99,
               itemHeight: 20,
               itemOpacity: 0.75,
+              itemTextColor: muiTheme.palette.text.primary,
               symbolSize: 9,
               symbolShape: "circle",
               symbolBorderColor: "rgba(0, 0, 0, .5)",
+              onClick: (d) => {
+                if (!active) {
+                  const colorList = data.map((series) =>
+                    series.id === d.id
+                      ? d.color
+                      : meData && meData.me && meData?.me.theme === "light"
+                      ? blueGrey[100]
+                      : blueGrey[800]
+                  );
+                  setColors(colorList);
+                  setActive(true);
+                } else {
+                  setActive(false);
+                }
+              },
               effects: [
                 {
                   on: "hover",
